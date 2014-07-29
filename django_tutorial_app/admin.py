1cc10963ff28061112074abb231a10a6094a3256
ahcub <olex.buchkovsky@gmail.com>
2014-07-29 18:05:07 +0300

    add admin management part of django tutorial

--- django_tutorial_app/admin.py
+++ django_tutorial_app/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from django_tutorial_app.models import Poll
+from django_tutorial_app.models import Poll, Choice
 
 
-admin.site.register(Poll)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
